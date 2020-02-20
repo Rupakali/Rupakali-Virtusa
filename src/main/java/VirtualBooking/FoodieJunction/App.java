8780d118ec78dbe1b22dfbc4f329b00271b354b8
Rupakali <rupakali19@gmail.com>
2020-02-20 12:33:14 +0545

    First

---
+++ src/main/java/VirtualBooking/FoodieJunction/App.java
@@ -0,0 +1,13 @@
+package VirtualBooking.FoodieJunction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
